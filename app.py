aa8e708d16595f4ce349de43b300d7f7cf6dc3c7
Aman <amanprocoder@gmail.com>
2024-08-15 16:35:07 +0530

    updating

--- app.py
+++ app.py
@@ -42,5 +42,7 @@ def content_based_model():
     return recommendations
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
