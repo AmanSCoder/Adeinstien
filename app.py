b02307c2c0a09efec0ccc24bcc758edca9ee5fa1
Aman <amanprocoder@gmail.com>
2024-08-15 12:53:10 +0530

    adding models

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def print_cluster(i):
     cluster_terms = [terms[ind] for ind in order_centroids[i, :10]]
     return cluster_terms
 
-@app.route('/recommend', methods=['POST'])
-def recommend():
+@app.route('/recommend/reinforcement_model', methods=['POST'])
+def reinforcement_model():
     data = request.get_json()
     product_description = data['product_description']
     
@@ -29,6 +29,20 @@ def recommend():
     return jsonify({
         'cluster': int(prediction[0]),
         'recommendations': recommendations
+    })
+
+def content_based_recommendations(desc):
+    
+
+    return "soap"
+
+@app.route('/recommend/content_based_model',method=['POST'])
+def content_based_model():
+    data=request.get_json()
+    product_description=data['product_description']
+    recommendations=content_based_recommendations(product_description)
+    return jsonify({
+        'recommendations':recommendations
     })
 
 if __name__ == '__main__':
