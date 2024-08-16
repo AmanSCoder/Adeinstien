dc8da6fd9f978b149d68b61d02b93f2663bb7c7c
Aman <amanprocoder@gmail.com>
2024-08-17 01:24:56 +0530

    adding upload file

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+import pickle
+
+import content_based_recommendation
+
+import warnings
+
+# Suppress all warnings
+warnings.filterwarnings('ignore')
+
+# Load the model and vectorizer
+with open('model/reinforcement_model/kmeans_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('model/reinforcement_model/tfidf_vectorizer.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+    return 'Success', 200
+
+
+def print_cluster(i):
+    terms = vectorizer.get_feature_names_out()
+    order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+    cluster_terms = [terms[ind] for ind in order_centroids[i, :10]]
+    return cluster_terms
+
+@app.route('/recommend/reinforcement_model', methods=['POST'])
+def reinforcement_model():
+    data = request.get_json()
+    product_description = data['product_description']
+    
+    Y = vectorizer.transform([product_description])
+    prediction = model.predict(Y)
+    
+    recommendations = print_cluster(prediction[0])
+    
+    return jsonify({
+        'cluster': int(prediction[0]),
+        'recommendations': recommendations
+    })
+
+
+@app.route('/recommend/content_based_model',methods=['POST'])
+def content_based_model():
+    desc=request.get_json()
+    description = desc['description']
+    recommendations=content_based_recommendation.recommend_products(description)
+    return recommendations
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
